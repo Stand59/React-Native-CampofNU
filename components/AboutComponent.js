1889e2c0618ed94489bdc405c89896f30376dfe3
NathanStanding <standingtall1000@gmail.com>
2022-01-26 12:33:26 -0700

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -4,11 +4,14 @@ import { Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 
+import Loading from './LoadingComponent';
+
 const mapStateToProps = state => {
     return {
         partners: state.partners
     };
 };
+
 
 function Mission() {
     return (
@@ -39,16 +42,40 @@ class About extends Component {
             );
         };
 
+        if (this.props.partners.isLoading) {
+            return (
+                <ScrollView>
+                <Mission />
+                <Card
+                    title='Community Partners'>
+                    <Loading />
+                </Card>
+            </ScrollView>
+            )
+        }
+
+        if (this.props.partners.errMess) {
+            return (
+                <ScrollView>
+                <Mission />
+                <Card
+                    title='Community Partners'>
+                    <Text>{this.props.partners.errMess}</Text>
+                </Card>
+            </ScrollView>
+            )
+        }
+
         return (
             <ScrollView>
                 <Mission />
                 <Card
                     title='Community Partners'>
-                        <FlatList
-                            data={this.props.partners.partners}
-                            renderItem={renderPartner}
-                            keyExtractor={item => item.id.toString()}
-                        />
+                    <FlatList
+                        data={this.props.partners.partners}
+                        renderItem={renderPartner}
+                        keyExtractor={item => item.id.toString()}
+                    />
                 </Card>
             </ScrollView>
         );
